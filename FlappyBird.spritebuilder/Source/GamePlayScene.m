ea4b41b4d5c23d1b9248fae6476b30f89cc1bc26
vincentldp <vincentlouvart@gmail.com>
2014-11-04 17:37:35 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,6 +14,17 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f) {
+        //Add a new obstacle
+        [self addObstacle];
+        
+        //Then reset the timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
@@ -22,6 +33,7 @@
     // this will get called every time the player touches the screen
     [character flap];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 
